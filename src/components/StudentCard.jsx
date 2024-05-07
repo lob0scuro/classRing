3be6a302aeb772cb14be05def4d5dea3fcf90474
lob0scuro <kamrin717@gmail.com>
2024-05-07 15:38:13 -0500

    continuing work on student cars, need to figure out how to auto populate card colors based off of form inputs

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import styles from "./student-card.module.css";
+import PointsButtonBlock from "./Buttons/PointsButtonBlock";
 
 const StudentCard = () => {
+  const svgStyles = {
+    width: "100%",
+    height: "100%",
+    fill: "none",
+    stroke: "var(--saturatedPink)",
+    strokeWidth: "25",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+    strokeMiterlimit: "10",
+    strokeDasharray: "0",
+    strokeDashoffset: "0",
+    opacity: "1",
+  };
+
   return (
     <div className={styles.studentCard}>
-      <div className={styles.circle}></div>
+      <div className={styles.circle}>
+        <svg style={svgStyles} width="100%" height="100%">
+          <circle r="35%" cx="50%" cy="50%" />
+        </svg>
+      </div>
       <div className={styles.infoBlock}>
         <h5>Claire</h5>
-        <div className={styles.controllerBlock}>
-          <button>-10</button>
-          <button>-5</button>
-          <button>+5</button>
-          <button>+10</button>
-        </div>
+        <PointsButtonBlock />
       </div>
     </div>
   );
