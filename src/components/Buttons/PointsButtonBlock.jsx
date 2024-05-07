f8482f1bf671ddcd9a71e9b7035b9cdc825154cf
lob0scuro <kamrin717@gmail.com>
2024-05-07 15:38:39 -0500

    recommit

---
+++ src/components/Buttons/PointsButtonBlock.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./button-bar.module.css";
+
+const PointsButtonBlock = () => {
+  const buttonStyles = {
+    backgroundColor: "var(--pastelPink)",
+    fontSize: "1.2rem",
+    fontWeight: "600",
+    padding: "8px",
+    color: "var(--saturatedPink)",
+  };
+  return (
+    <div className={styles.btnBlock}>
+      <button className={styles.btn} style={buttonStyles}>
+        -10
+      </button>
+      <button className={styles.btn} style={buttonStyles}>
+        -5
+      </button>
+      <button className={styles.btn} style={buttonStyles}>
+        +5
+      </button>
+      <button className={styles.btn} style={buttonStyles}>
+        +10
+      </button>
+    </div>
+  );
+};
+
+export default PointsButtonBlock;
